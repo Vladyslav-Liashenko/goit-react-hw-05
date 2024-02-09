6807cb0e26f3526e9f0e45b422538c8fc97a9103
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-02-09 13:04:24 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
+import { Link, Routes, Route } from 'react-router-dom';
 // import { useState } from 'react';
 import './App.css';
+// import axios from 'axios';
+
+// http://localhost:5173/
+// http://localhost:5173/Movies
 
 export const App = () => {
-
   return (
     <div>
-      lox
-    </div>
-  )
-}
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
 
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/movies" element={<div>Movies</div>} />
+        <Route path="*" element={<div>Not found page</div>} />
+      </Routes>
+    </div>
+  );
+};
