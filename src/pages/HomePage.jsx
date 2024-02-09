dd82d8a8552d84aec106f188bf67789243128502
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-02-09 19:18:55 +0200

    saved

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { fetchPopular } from '../components/Services/Api';
+import { fetchPopular } from '../components/Services/api';
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,4 +1,4 @@
-import { fetchSearch } from '../components/Services/Api';
+import { fetchSearch } from '../components/Services/api';
 import { useState, useEffect } from 'react';
 import { SearchBar } from '../components/SearchBar/SearchBar';
 import { Link, useLocation } from 'react-router-dom';
