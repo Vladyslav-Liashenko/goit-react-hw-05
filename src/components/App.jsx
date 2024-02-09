98ba21325b525d25e0b8ef96801082a91b1e0c8f
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-02-09 14:54:31 +0200

    saved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './NavBar/NavBar';
+import MoviesPage from '../pages/MoviesPage';
+import HomePage from '../pages/HomePage';
 
 // import { useState } from 'react';
 // import axios from 'axios';
@@ -11,8 +13,8 @@ export const App = () => {
       <NavBar />
 
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/movies" element={<div>Movies</div>} />
+        <Route path="/" element={<div><HomePage/></div>} />
+        <Route path="/movies" element={<div><MoviesPage/></div>} />
         <Route path="*" element={<div>Not found page</div>} />
       </Routes>
     </div>
